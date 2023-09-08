4e3f5d1c520cd7bc06be99e40b9b787a981ef843
Chris Mineweaser <cmineweaser@hotmail.com>
2023-09-08 14:55:26 -0400

    Rocket straifing

--- Assets/Scripts/RocketLaunch.cs
+++ Assets/Scripts/RocketLaunch.cs
@@ -8,34 +8,43 @@ public class RocketLaunch : MonoBehaviour
     [Header("Rocket Settings")]
     public float MaxHeight;
     public float MaxSpeed; // Maximum speed the rocket can have
+    public float StrafeSpeed; // Speed for moving rocket left and right
 
     [Header("Quiz Settings")]
     public int CorrectAnswers;
-    
+
     private float targetHeight;
     private bool launch = false;
     private bool falling = false;
     private Vector3 startPos;
     private Rigidbody2D rb;
     private float speed; // This is now private as its value is calculated based on MaxSpeed and CorrectAnswers
+    private float leftBound; // To store the leftmost x value the rocket can be
+    private float rightBound; // To store the rightmost x value the rocket can be
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         startPos = transform.position;
         rb.gravityScale = 0;
+
+        // Calculate camera bounds:
+        float camHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        leftBound = Camera.main.transform.position.x - camHalfWidth;
+        rightBound = Camera.main.transform.position.x + camHalfWidth;
     }
 
     void Update()
     {
         CorrectAnswers = GameData.Instance.score;
-        
+
         if (Input.GetKeyDown(KeyCode.Space) && !launch)
         {
             targetHeight = (MaxHeight / 21) * CorrectAnswers;
             speed = (MaxSpeed / 20) * CorrectAnswers; // Calculate the speed
             launch = true;
         }
+
         if (launch && transform.position.y < targetHeight)
         {
             rb.velocity = Vector2.up * speed; // Use the calculated speed here
@@ -60,6 +69,18 @@ public class RocketLaunch : MonoBehaviour
             transform.position = new Vector3(transform.position.x, startPos.y, transform.position.z);
             // Wait for 3 seconds, then load the "QuestionsScene"
             StartCoroutine(LoadQuestionScene());
+        }
+
+        // Strafing:
+        if (launch || falling) // Check if the rocket is in the air
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            if(Mathf.Abs(horizontalInput) > 0.1f)
+            {
+                Vector3 newPosition = transform.position + new Vector3(horizontalInput * StrafeSpeed * Time.deltaTime, 0, 0);
+                newPosition.x = Mathf.Clamp(newPosition.x, leftBound, rightBound); // Make sure the rocket doesn't go out of the camera bounds
+                transform.position = newPosition;
+            }
         }
     }
 
